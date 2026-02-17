1ace97e2a1bb4761f7543c32b00b3686d792b356
MariaVilma12 <vilmashekar26@gmail.com>
2026-02-17 18:08:32 +0100

    inheritance example

---
+++ inheritance/People/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using Inheritance.School;
+
+namespace Inheritance.People;
+
+public class Teacher:Person
+{
+    private Subject _subject;
+    
+    public Teacher(string firstName, string lastName, Subject subject) : base(firstName, lastName)
+    {
+        this._subject = subject;
+    }
+
+    public Subject Subject => _subject;
+
+    public string GetInfoOld()
+    {
+        return $"{_firstName} {_lastName} teaches {_subject}.";
+    }
+    
+    /// <summary>
+    /// teacher info
+    /// </summary>
+    /// <returns></returns>
+    public override string GetInfo()
+    {
+        return GetInfoOld();
+    }
+}
